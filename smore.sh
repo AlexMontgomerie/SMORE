3f712c36e4cefe92fe11555da40356ebdf19f39a
AlexMontgomerie <am9215@ic.ac.uk>
2019-10-30 20:01:03 +0000

    its working!

--- smore.sh
+++ smore.sh
@@ -4,18 +4,29 @@ SMORE_PATH=~/SMORE
 
 # 1 - Chose a random pair of keys
 KEYS_ALL=( $(cat $SMORE_PATH/KEYS.txt) )
-KEY_ORIGINAL=${KEYS_ALL[$(($RANDOM % ${#KEYS_ALL[@]}))]}
-KEY_SWAP=${KEYS_ALL[$(($RANDOM % ${#KEYS_ALL[@]}))]}
+KEYCODE_ORIGINAL=${KEYS_ALL[$(($RANDOM % ${#KEYS_ALL[@]}))]}
+KEYCODE_SWAP=${KEYS_ALL[$(($RANDOM % ${#KEYS_ALL[@]}))]}
 
 # 2 - Change key mapping
-echo "TODO: change key mapping"
+KEYVALUE_ORIGINAL=$(xmodmap -pke | grep "^keycode[[:space:]]\{1,\}$KEYCODE_ORIGINAL = ")
+KEYVALUE_SWAP=$(xmodmap -pke | grep "^keycode[[:space:]]\{1,\}$KEYCODE_SWAP = ")
+
+KEYVALUE_ORIGINAL=${KEYVALUE_ORIGINAL/$KEYCODE_ORIGINAL/$KEYCODE_SWAP}
+KEYVALUE_SWAP=${KEYVALUE_SWAP/$KEYCODE_SWAP/$KEYCODE_ORIGINAL}
+
+xmodmap -e "${KEYVALUE_ORIGINAL}"
+xmodmap -e "${KEYVALUE_SWAP}"
 
 # 3 - Wait for key stroke
 xinput list | grep -Po 'id=\K\d+(?=.*slave\s*keyboard)' | xargs -P0 -n1 xinput test | \
-    $SMORE_PATH/smore_check.sh $KEY_ORIGINAL $KEY_SWAP  
+    $SMORE_PATH/smore_check.sh $KEYCODE_ORIGINAL $KEYCODE_SWAP  
 
 # 4 - Change key mapping
 echo "TODO: reset key mapping"
 
+KEYVALUE_ORIGINAL=${KEYVALUE_ORIGINAL/$KEYCODE_SWAP/$KEYCODE_ORIGINAL}
+KEYVALUE_SWAP=${KEYVALUE_SWAP/$KEYCODE_ORIGINAL/$KEYCODE_SWAP}
 
+xmodmap -e "${KEYVALUE_ORIGINAL}"
+xmodmap -e "${KEYVALUE_SWAP}"
 
