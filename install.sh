e59f8d58ab83bbc42332b52ed1491c07810f0543
Yann Herklotz <git@yannherklotz.com>
2019-11-06 19:40:58 +0000

    Everything working

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ sudo cp smore_check.sh /etc/smore/.
 sudo cp KEYS.txt /etc/smore/.
 sudo chown $USER:$USER -R /etc/smore
 cp smore.service $HOME/.config/systemd/user/.
+cp restart_smore.service $HOME/.config/systemd/user/.
 cp smore.timer $HOME/.config/systemd/user/.
 xmodmap -pke > /etc/smore/.Xmodmap
--- smore.sh
+++ smore.sh
@@ -5,6 +5,7 @@ set -x
 SMORE_PATH=/etc/smore
 # 0 - Reset key mapping
 # xmodmap $SMORE_PATH/.Xmodmap
+if [[ -f "$SMORE_PATH/LAST_KEYS" ]]; then xmodmap "$SMORE_PATH/LAST_KEYS"; fi
 
 # 1 - Chose a random pair of keys
 KEYS_ALL=( {9..66} ) #KEYS_ALL=( $(cat $SMORE_PATH/KEYS.txt) )
@@ -15,6 +16,11 @@ KEYCODE_SWAP=${KEYS_ALL[$(($RANDOM % ${#KEYS_ALL[@]}))]}
 KEYVALUE_ORIGINAL=$(xmodmap -pke | grep "^keycode[[:space:]]\{1,\}$KEYCODE_ORIGINAL = ")
 KEYVALUE_SWAP=$(xmodmap -pke | grep "^keycode[[:space:]]\{1,\}$KEYCODE_SWAP = ")
 
+cat >$SMORE_PATH/LAST_KEYS <<EOF
+${KEYVALUE_ORIGINAL/$KEYCODE_SWAP/$KEYCODE_ORIGINAL}
+${KEYVALUE_SWAP/$KEYCODE_ORIGINAL/$KEYCODE_SWAP}
+EOF
+
 KEYVALUE_ORIGINAL=${KEYVALUE_ORIGINAL/$KEYCODE_ORIGINAL/$KEYCODE_SWAP}
 KEYVALUE_SWAP=${KEYVALUE_SWAP/$KEYCODE_SWAP/$KEYCODE_ORIGINAL}
 
@@ -24,11 +30,4 @@ xmodmap -e "${KEYVALUE_SWAP}"
 # 3 - Wait for key stroke
 xinput list | grep -Po 'id=\K\d+(?=.*slave\s*keyboard)' | xargs -P0 -n1 xinput test | \
     $SMORE_PATH/smore_check.sh "$KEYCODE_ORIGINAL" "$KEYCODE_SWAP" "$KEYVALUE_ORIGINAL" "$KEYVALUE_SWAP"
-
-# 4 - Change key mapping
-# KEYVALUE_ORIGINAL=${KEYVALUE_ORIGINAL/$KEYCODE_SWAP/$KEYCODE_ORIGINAL}
-# KEYVALUE_SWAP=${KEYVALUE_SWAP/$KEYCODE_ORIGINAL/$KEYCODE_SWAP}
-# 
-# xmodmap -e "${KEYVALUE_ORIGINAL}"
-# xmodmap -e "${KEYVALUE_SWAP}"
 
