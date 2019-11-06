286e664c6099fac7d143c2859742f71ed22a9aef
Yann Herklotz <git@yannherklotz.com>
2019-11-06 20:11:47 +0000

    Don't call sudo that many times

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo mkdir -p /etc/smore
-sudo cp smore.sh /etc/smore/.
-sudo cp smore_check.sh /etc/smore/.
-sudo cp KEYS.txt /etc/smore/.
 sudo chown $USER:$USER -R /etc/smore
+cp smore.sh /etc/smore/.
+cp smore_check.sh /etc/smore/.
+cp KEYS.txt /etc/smore/.
 cp smore.service $HOME/.config/systemd/user/.
 cp restart_smore.service $HOME/.config/systemd/user/.
 cp smore.timer $HOME/.config/systemd/user/.
