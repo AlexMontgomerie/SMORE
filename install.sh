08788462a22d821fa3774db2618ce428c8649da4
adityarajagopal <ar4414@ic.ac.uk>
2019-11-08 16:06:53 +0000

    up and running with enable instead of start

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 sudo mkdir -p /etc/smore
 sudo chown $USER:$USER -R /etc/smore
-cp smore.sh /etc/smore/.
+cp smore /etc/smore/.
 cp smore.service $HOME/.config/systemd/user/.
 cp restart_smore.service $HOME/.config/systemd/user/.
 cp smore.timer $HOME/.config/systemd/user/.
 xmodmap -pke > /etc/smore/.Xmodmap
 systemctl --user daemon-reload
-systemctl --user start smore.service
-systemctl --user start smore.timer
+systemctl --user enable smore.service
+systemctl --user enable smore.timer
+
